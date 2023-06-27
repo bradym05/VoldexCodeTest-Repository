aef587ba4831c2ac5c19289084fc09c43ec5a488
bradym05 <78286012+bradym05@users.noreply.github.com>
2023-06-27 17:07:07 -0600

    New PlayerData module and updated README
    I created a base for PlayerData which allows me to save data and easily handle it. I updated the README to include some additional details which I have observed.

--- README.md
+++ README.md
@@ -2,11 +2,26 @@
  
 Initial Gameplay Analysis:
 
--In the output, many Sound objects throw the error "failed to load". These should be deleted to avoid clutter and unnecessary memory usage.
--The first two buttons have no label to indicate their purpose.
--The money display GUI appears underneath Roblox's CoreGui, this should be moved to somewhere more visible.
--The "PlayerData" script throws the error "attempt to index nil with UserId" on line 26.
--Button label stays after purchasing an item even though the button disappears.
--Area behind the portal is unobtainable.
--Buildings can be purchased even without sufficient funds leading to a negative money count.
--On the second test run, the money counter GUI does not update.
+-In the output, many Sound objects throw the error "failed to load". These should be deleted to avoid clutter and unnecessary memory usage. <br />
+-The first two buttons have no label to indicate their purpose. <br />
+-The money display GUI appears underneath Roblox's CoreGui, this should be moved to somewhere more visible. <br />
+-The "PlayerData" script throws the error "attempt to index nil with UserId" on line 26. <br />
+-Button label stays after purchasing an item even though the button disappears. <br />
+-Area behind the portal is unobtainable. <br />
+-Buildings can be purchased even without sufficient funds leading to a negative money count. <br />
+-On the second test run, the money counter GUI does not update. <br />
+
+Other Notes:<br />
+
+-ServerStorage is almost entirely unused. ReplicatedStorage should only be used for items which need to be replicated to the client. <br />
+-The code indexes services instead of using game:GetService(), this should be modified <br />
+-Code needs to be commented to improve readability <br />
+
+PadService:<br />
+
+The PadPurchase event is redundant. onPadPurchased can simply be called from the line that PadPurchase is fired on. onPadPurchased also appears unnecessary,
+it initalizes a "message" variable but does nothing with it.
+
+PlayerData:<br />
+
+I am going to entirely refactor this script. Many of the functions are redundant and I would like to save player data to DataStores.
--- src/ServerScriptService/PlayerData.lua
+++ src/ServerScriptService/PlayerData.lua
@@ -1,67 +1,171 @@
-local PlayerData = {}
+--------// VARIABLES \\--------
 
+--Services
 local Players = game:GetService("Players")
+local DataStoreService = game:GetService("DataStoreService")
 
-local UpdateUI: RemoteEvent = game.ReplicatedStorage.Remotes.UpdateUI
-local data = {}
+local options = Instance.new("DataStoreOptions")
+local data = DataStoreService:GetDataStore("Save1")
 
-local defaultData = {
-	money = 1000,
-	paycheck = 100,
-	paycheckWithdrawAmount = 0,
-	padsPurchased = {}
+--String keys to indexes
+local keys = {
+	Money = 1,
 }
 
-function PlayerData:GetPaycheckWithdrawAmount(player)
-	local data = PlayerData:GetPlayerData(player)
-	return data.paycheckWithdrawAmount
+--Default data
+local defaultData = {1000, 100, 0, {}} --Money, paycheck, paycheckWithdrawAmount, padsPurchased
+
+----// PRIVATE FUNCTIONS \\----
+
+--DataStore error codes and corresponding callbacks
+local codeCallbacks = {}
+
+--Callback for throughput errors
+local function yield()
+	task.wait(5)
 end
 
-function PlayerData:SetPaycheckWithdrawAmount(player, amount)
-	local data = PlayerData:GetPlayerData(player)
-	data.paycheckWithdrawAmount = amount
+--Throughput errors range from 301 - 306, assign to yield callback
+for i = 301,306 do
+	codeCallbacks[tostring(i)] = yield
 end
 
-function PlayerData:GetPlayerData(player)
-	return data[player.UserId]
+--Safely handles DataStoreService requests and responds accordingly to errors
+local function safeManage(functionName,...)
+	--Initialize variables
+	local succ, result, errorCode
+	local arguments = {...}
+
+	repeat
+		succ, result, errorCode = pcall(function()
+			--Call the datastore function with given arguments
+			data[functionName](data, unpack(arguments))
+		end)
+		--Catch any errors: https://create.roblox.com/docs/cloud-services/datastores#error-codes
+		if not succ then
+			--See if a callback exists
+			if codeCallbacks[tostring(result)] then
+				--Run callback function
+				codeCallbacks[tostring(result)]()
+			else
+				--Break loop if error has no callback
+				break
+			end
+		end
+	until succ
+
+	--Return final result
+	return result
 end
 
-function PlayerData:GetMoney(player, amount)
-	local data = PlayerData:GetPlayerData(player)
-	return data.money
+-------// DATA CLASS \\-------
+
+local PlayerData = {}
+PlayerData.__index = PlayerData
+
+--Responsible for loading new players
+function PlayerData.new(Player : PLayer)
+	--Get player data with UserId as the key
+	local loadedData = safeManage("GetAsync",tostring(Player.UserId))
+	--Called function yields, so check that Player is still in-game
+	if Player and Player:IsDescendantOf(Players) then
+		--Reconcile
+		if not loadedData then
+			loadedData = defaultData
+		end
+		--Create object and return
+		local self = {}
+		self.Data = loadedData
+		self.Player = Player
+		self.Key = tostring(Player.UserId)
+		self.Changed = false
+
+		setmetatable(self, PlayerData)
+		return self
+	end
+	--Something went wrong, return false
+	return false
 end
 
-function PlayerData:SetMoney(player, amount)
-	local data = PlayerData:GetPlayerData(player)
-	data.money = amount
-	UpdateUI:FireClient(player, data.money)
+--Increment data
+function PlayerData:Increment(key, value, inc)
+	--Convert to index
+	key = keys[key]
+	--Make sure index exists
+	if key then
+		--Reconcile if data does not exist
+		if not self.Data[key] then
+			self.Data[key] = inc
+		else
+			self.Data[key] += inc
+		end
+		--Indicate data has changed
+		self.Changed = true
+	end
 end
 
-function PlayerData:GetPaycheck(player)
-	local data = PlayerData:GetPlayerData(player)
-	return data.paycheck
+--Cleans up PlayerData objects and saves
+function PlayerData:Destroy()
+	safeManage("SetAsync", self.Key, self.Data)
+	table.clear(self)
+	setmetatable(self,nil)
+	table.freeze(self)
 end
 
-function PlayerData:SetPaycheck(player, amount)
-	local data = PlayerData:GetPlayerData(player)
-	data.paycheck = amount
-end
+-------// DATA MANAGER \\-------
 
-function PlayerData:AddMoney(player, amount)
-	local data = PlayerData:GetPlayerData(player)
-	data.money += amount
-	UpdateUI:FireClient(player, data.money)
-end
+--Event instance to signal player data has loaded
+local loadedEvent = Instance.new("BindableEvent")
+--Declare class with initialized variables
+local DataManager = {
+	PlayerLoaded = loadedEvent.Event,
+	PlayerSaves = {},
+}
 
-function PlayerData:SubtractMoney(player, amount)
-	local data = PlayerData:GetPlayerData(player)
-	data.money -= amount
-	UpdateUI:FireClient(player, data.money)
+--Load added players
+local function playerAdded(Player : Player)
+	--Create a new data object
+	local result = PlayerData.new(Player)
+	--Make sure player successfully loaded
+	if result then
+		--Refer player to data object inside of PlayerSaves
+		DataManager.PlayerSaves[Player] = result
+		--Signal that the player loaded and send their data object
+		loadedEvent:Fire(Player, result)
+	end
 end
+Players.PlayerAdded:Connect(playerAdded)
 
-Players.PlayerAdded:Connect(function(player)
-	data[player.UserId] = defaultData
+--Clean up players once they leave
+Players.PlayerRemoving:Connect(function(Player : Player)
+	--Check if player has any stored reference
+	if DataManager.PlayerSaves[Player] then
+		--Clean up
+		DataManager.PlayerSaves[Player]:Destroy()
+		DataManager.PlayerSaves[Player] = nil
+	end
 end)
 
+--Catch any players who may have already joined
+for _, Player : Player in pairs(Players:GetPlayers()) do
+	task.spawn(playerAdded, Player)
+end
 
-return PlayerData
+--Save all updated data in 30 second intervals
+task.spawn(function()
+	while true do
+		task.wait(30)
+		--Loop through all players
+		for player, dataObject in pairs(DataManager.PlayerSaves) do
+			--See if any changes have occurred
+			if dataObject.Changed == true then
+				--Update to indicate data has been saved
+				dataObject.Changed = false
+				--Save data
+				safeManage("SetAsync", dataObject.Key, dataObject.Data)
+			end
+		end
+	end
+end)
+
+return DataManager
